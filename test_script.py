fc68a2ae0218a250eb196347692a2913799060aa
buiandrew <aphibui@gmail.com>
2023-09-12 14:24:10 -0700

    Create test_script.py
    create test script

---
+++ test_script.py
@@ -0,0 +1,14 @@
+# test_script.py
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    result_add = add(5, 3)
+    result_subtract = subtract(10, 4)
+    
+    print(f"Addition result: {result_add}")
+    print(f"Subtraction result: {result_subtract}")
